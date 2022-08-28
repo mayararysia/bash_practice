1461afa77c7aac9f77812461f3895d2bf5418c73
mayararysia <mayara.ryzia@gmail.com>
2022-08-28 18:06:31 -0300

    removed > t

--- first_test/02.sh
+++ first_test/02.sh
@@ -1,3 +1,3 @@
 #Exiba o código dos produto que NÂO são da marca SONY
 
-tail -n 1181 lista_first_test.txt | grep -vi "sony" | awk '{print $1}' > t.txt
+tail -n 1181 lista_first_test.txt | grep -vi "sony" | awk '{print $1}'
